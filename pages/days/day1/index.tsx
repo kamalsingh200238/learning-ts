2beef1afaa14a552ece82c122daab5076abd8cdc
Kamal Singh <kamalsingh200238@gmail.com>
2023-04-11 13:20:25 +0530

    feat: create 2 classes where SuperHuman extends Human

--- pages/days/day1/index.tsx
+++ pages/days/day1/index.tsx
@@ -1,6 +1,29 @@
 import Head from "next/head";
 
 export default function Day1() {
+
+  class Human {
+    talk() {
+      return "Human talking\n"
+    }
+  }
+
+  class SuperHuman extends Human {
+    fly() {
+      return "Flying....\n"
+    }
+  }
+
+  const firstPerson = new Human();
+  const secondPerson = new SuperHuman();
+
+  console.log({firstPerson, secondPerson})
+
+  console.log(
+    firstPerson.talk(),
+    secondPerson.talk(),
+    secondPerson.fly(),
+  )
   return (
     <>
       <Head>
