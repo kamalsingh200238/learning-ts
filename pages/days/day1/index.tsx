f45d371a4986f74e63267a6d05a0c77363d7eeef
Kamal Singh <kamalsingh200238@gmail.com>
2023-04-11 13:05:43 +0530

    chore: created new routes

---
+++ pages/days/day1/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Day1() {
+  return (
+    <>
+      <Head>
+        <title>Classes and Inheritence</title>
+      </Head>
+      <div>Day1</div>
+    </>
+  );
+}
